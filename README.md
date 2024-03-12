89a127474883dda837a5342d36aa11e6f717cd52
Anna Gabriela <124202500+annaagabi@users.noreply.github.com>
2024-03-12 19:00:01 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+# Tarefandoo - TODO List
+Descrição... API - Back-End
+
+## 1. Criando:
+**No terminal (Back):**
+- [x] Inicializando o projeto (criar o package. json): `npm init -y`
+- [x] `npm i express mysql2 cors nodemon​ dotenv`
+- [x] `npm i bcrypt`
+- [x] `npm i -D @types/bcrypt`
+- [x] `npm i `
+- [x] `npm i`
+
+## 2. Adicionar ao Script no Package.JSON:
+- [x] Adicione ao package.json, na parte de “script” o: "start": "nodemon ./index.js localhost 3000"
+```js
+"scripts": {
+  "test": "echo \"Error: no test specified\" && exit 1",
+  "start": "nodemon ./index.js localhost 3000"
+},
+```
+
+
+## 7. Endpoints
+### 7.1 Para Verificar se a API Está Funcionando
+- [x] {{URL}}/
+- [x] https://todo-xfvj.onrender.com/
+
+
---
+++ signup.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 12-Mar-2024 às 22:55
+-- Versão do servidor: 10.4.32-MariaDB
+-- versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `signup`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `name` varchar(220) NOT NULL,
+  `email` varchar(220) NOT NULL,
+  `password` varchar(220) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `login`
+--
+
+INSERT INTO `login` (`id`, `name`, `email`, `password`) VALUES
+(1, 'anna', 'anna@gmail.com', '154156415'),
+(2, 'douglas', 'douglas@gmail.com', 'dyhsfguysdgfyhs'),
+(3, 'um', 'hum@gmail.com', 'ahdgasgd'),
+(4, 'teste', 'teste@gmail.com', 'testando'),
+(5, 'atualizou', 'atualizacao@gmail.com', 'adhgasg'),
+(7, 'deu certo?', 'sim@gmail.com', 'aaaaaaaaaaaaa'),
+(8, 'senha', 'adaf@gmail.com', 'assenha'),
+(9, 'sgadhgasdgh', 'sgadhgasdgh@gmail.com', 'A*1aaasgadhgasdgh'),
+(10, 'hash', 'hash@gmail.com', '$2b$10$oSTW1CptHOMVdRE3De3yYOyHw6O3OkWQCe9sx5bHFotVFqYJNa7Ny');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
